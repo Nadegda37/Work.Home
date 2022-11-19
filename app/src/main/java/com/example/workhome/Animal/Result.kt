41c35f0b61707b2eaa8b185543dfdbb1c218170a
meekr <missis.nadegda2010ivanovo@yandex.ru>
2022-11-19 16:41:34 +0300

    Init

--- app/src/main/java/com/example/workhome/Animal/Result.kt
+++ app/src/main/java/com/example/workhome/Animal/Result.kt
@@ -8,17 +8,24 @@ fun main() {
     val home= arrayOf("Cow", "Goat")
     for (home in home)
         println("$home")
+    val listHome: List<String> = listOf("Cow", "Goat")
 
     val wild = arrayOf("Elephant", "Giraffe")
     for (wild in wild)
         println("$wild")
+    val list2: List<String> = listOf("Elephant", "Giraffe")
+        println("Herbivorous: $list2, $listHome")
+
 
     val predator = arrayOf("Hyena", "Lynx")
     for (predator in predator)
         println("$predator")
+    val list3: List<String> = listOf("Hyena", "Lynx")
 
     val mammals = arrayOf("Dolphin", "Mink")
     for (mammals in mammals)
         println("$mammals")
+    val list4: List<String> = listOf("Dolphin", "Mink")
+        println("Carnevorous: $list4, $list3")
 
 }
